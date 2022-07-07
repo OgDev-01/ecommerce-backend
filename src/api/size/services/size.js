e9891b9189abfc8f1db41976a40b7320009a835f
Og-Dev <oliviamegan11@gmail.com>
2022-07-07 19:27:11 +0100

    [feat] strapi version upgrade to 4.2.2

--- src/api/size/services/size.js
+++ src/api/size/services/size.js
@@ -1,9 +1,9 @@
 'use strict';
 
 /**
- * page service.
+ * size service.
  */
 
 const { createCoreService } = require('@strapi/strapi').factories;
 
-module.exports = createCoreService('api::page.page');
+module.exports = createCoreService('api::size.size');
